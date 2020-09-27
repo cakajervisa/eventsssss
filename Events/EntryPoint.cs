ec8af8114e781bd42109ffe621777a20bd92deed
ervisaaa <ervisa.cakaj@gmail.com>
2020-09-27 23:18:31 +0200

    1c

---
+++ Events/EntryPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Events
+{
+    public class EntryPoint
+    {
+        static int score = 0;
+
+        static void Main(string[] args)
+        {
+            Shooter shooter = new Shooter();
+
+            shooter.ShotsFired += KilledEnemy;
+            //shooter.ShotsFired += KilledEnemy;
+            //shooter.ShotsFired += AddScore;
+
+            shooter.OnShoot();
+        }
+
+        // Subscriber methods to the event
+        static void KilledEnemy(object source, EventArgs e)
+        {
+            Console.WriteLine($"I killed an enemy ");
+  
+        }
+    
+
+
+
+
+        static void AddScore(object source, EventArgs e)
+        {
+            score++;
+        }
+
+      
+    }
+}
---
+++ Events/Shooter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Events
+{
+    public class Shooter
+    {
+
+        public delegate void KillingHandler(object sender, EventArgs e);
+        //public event KillingHandler ShotsFired;
+        public event EventHandler ShotsFired;
+        // Event to be invoked upon successful kill
+        //public event EventHandler<ShotsFiredEventArgs> ShotsFired;
+
+        private Random rng = new Random();
+
+        public void OnShoot()
+        {
+            while (true)
+            {
+                if (rng.Next(0, 100) % 2 == 0)
+                {
+                    ShotsFired.Invoke(this, EventArgs.Empty);
+                    //ShotsFired?.Invoke(this, new ShotsFiredEventArgs(DateTime.Now)); // EventName? means if Event is not Null (if it has subscribers)
+                }
+                else
+                {
+                    Console.WriteLine("I Missed!");
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
